32222415ea6f34f524128fbe2ecea296d769b0f9
Will Usher <wusher@kth.se>
2019-10-10 21:34:43 +0200

    Fix issue with fail update of decriptors

--- src/otoole/preprocess/create_datapackage.py
+++ src/otoole/preprocess/create_datapackage.py
@@ -16,47 +16,67 @@ from otoole.preprocess.longify_data import main as longify
 logger = logging.getLogger()
 
 
-def generate_package(path_to_simplicity):
-    """
+def generate_package(path_to_package):
+    """Creates a datapackage in folder ``path_to_package``
 
     [{'fields': 'REGION', 'reference': {'resource': 'REGION', 'fields': 'VALUE'}}]
     """
 
-    datapath = os.path.join(path_to_simplicity)
+    datapath = os.path.join(path_to_package)
     package = Package(base_path=datapath)
 
     package.infer('data/*.csv')
 
+    package.descriptor['licenses'] = [{"name": "CC-BY-4.0",
+                                       "path": "https://creativecommons.org/licenses/by/4.0/",
+                                       "title": "Creative Commons Attribution 4.0"}]
+
+    package.descriptor['title'] = 'The OSeMOSYS Simplicity Example Model'
+
+    package.descriptor['name'] = 'osemosys_model_simplicity'
+
+    package.descriptor['contributors'] = [{"title": "Will Usher",
+                                           "email": "wusher@kth.se",
+                                           "path": "http://www.kth.se/wusher",
+                                           "role": "author"
+                                           }]
+
+    package.commit()
+
     config = read_config()
 
+    new_resources = []
     for resource in package.resources:
+
+        descriptor = resource.descriptor
 
         name = resource.name
         if config[name]['type'] == 'param':
 
-            foreign_keys = []
-
             indices = config[name]['indices']
+            logger.debug("Indices of %s are %s", name, indices)
 
+            foreign_keys = []
             for index in indices:
-
                 key = {'fields': index, 'reference': {'resource': index, 'fields': 'VALUE'}}
-
                 foreign_keys.append(key)
 
-            schema = resource.schema
-            schema.descriptor['foreignKeys'] = foreign_keys
-            schema.descriptor['primaryKey'] = indices
-            schema.descriptor['missingValues'] = [str(config[name]['default'])]
-            schema.commit()
+            descriptor['schema']['foreignKeys'] = foreign_keys
+            descriptor['schema']['primaryKey'] = indices
+            descriptor['schema']['missingValues'] = [str(config[name]['default'])]
 
-    filepath = os.path.join(path_to_simplicity, 'datapackage.json')
+        new_resources.append(descriptor)
+
+    package.descriptor['resources'] = new_resources
+    package.commit()
+
+    filepath = os.path.join(path_to_package, 'datapackage.json')
     package.save(filepath)
 
 
-def validate_contents(path_to_data):
+def validate_contents(path_to_package):
 
-    filepath = os.path.join(path_to_data, 'datapackage.json')
+    filepath = os.path.join(path_to_package)
     package = Package(filepath)
 
     for resource in package.resources:
@@ -70,7 +90,8 @@ def validate_contents(path_to_data):
 def main(wide_folder, narrow_folder):
     longify(wide_folder, narrow_folder)
     generate_package(narrow_folder)
-    validate_contents(narrow_folder)
+    absolute_path = os.path.join(narrow_folder, 'datapackage.json')
+    validate_contents(absolute_path)
 
 
 if __name__ == '__main__':
