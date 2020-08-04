5e6bb1c7226b8fd50174c0220a73f870b5866ec1
Will Usher <wusher@kth.se>
2020-08-04 10:28:29 +0200

    Add strategy for reading folder of csvs

--- src/otoole/read_strategies.py
+++ src/otoole/read_strategies.py
@@ -1,4 +1,5 @@
 import logging
+import os
 from typing import Any, Dict, List, Tuple
 
 import pandas as pd
@@ -24,46 +25,7 @@ EXCEL_TO_CSV = {
 CSV_TO_EXCEL = {v: k for k, v in EXCEL_TO_CSV.items()}
 
 
-class ReadExcel(ReadStrategy):
-    def read(self, filepath) -> Tuple[Dict[str, pd.DataFrame], Dict[str, Any]]:
-
-        config = self.config
-        default_values = self._read_default_values(config)
-
-        xl = pd.ExcelFile(filepath)
-
-        input_data = {}
-
-        for name in xl.sheet_names:
-
-            try:
-                mod_name = EXCEL_TO_CSV[name]
-            except KeyError:
-                mod_name = name
-
-            config_details = config[mod_name]
-
-            df = xl.parse(name)
-
-            entity_type = config[mod_name]["type"]
-
-            if entity_type == "param":
-                narrow = self._check_parameter(df, config_details, mod_name)
-                if not narrow.empty:
-                    narrow_checked = check_datatypes(narrow, config, mod_name)
-                else:
-                    narrow_checked = narrow
-            elif entity_type == "set":
-                narrow = self._check_set(df, config_details, mod_name)
-                if not narrow.empty:
-                    narrow_checked = check_set_datatype(narrow, config, mod_name)
-                else:
-                    narrow_checked = narrow
-
-            input_data[mod_name] = narrow_checked
-
-        return input_data, default_values
-
+class ReadTabular(ReadStrategy):
     def _check_set(self, df, config_details, name):
 
         logger.info("Checking set %s", name)
@@ -110,9 +72,91 @@ class ReadExcel(ReadStrategy):
         return narrow[expected_headers]
 
 
-class ReadCsv(ReadStrategy):
+class ReadExcel(ReadTabular):
+    """Read in an Excel spreadsheet in wide format to a dict of Pandas DataFrames
+    """
+
     def read(self, filepath) -> Tuple[Dict[str, pd.DataFrame], Dict[str, Any]]:
-        raise NotImplementedError()
+
+        config = self.config
+        default_values = self._read_default_values(config)
+
+        xl = pd.ExcelFile(filepath)
+
+        input_data = {}
+
+        for name in xl.sheet_names:
+
+            try:
+                mod_name = EXCEL_TO_CSV[name]
+            except KeyError:
+                mod_name = name
+
+            config_details = config[mod_name]
+
+            df = xl.parse(name)
+
+            entity_type = config[mod_name]["type"]
+
+            if entity_type == "param":
+                narrow = self._check_parameter(df, config_details, mod_name)
+                if not narrow.empty:
+                    narrow_checked = check_datatypes(narrow, config, mod_name)
+                else:
+                    narrow_checked = narrow
+            elif entity_type == "set":
+                narrow = self._check_set(df, config_details, mod_name)
+                if not narrow.empty:
+                    narrow_checked = check_set_datatype(narrow, config, mod_name)
+                else:
+                    narrow_checked = narrow
+
+            input_data[mod_name] = narrow_checked
+
+        return input_data, default_values
+
+
+class ReadCsv(ReadTabular):
+    """Read in a folder of CSV files"""
+
+    def read(self, filepath) -> Tuple[Dict[str, pd.DataFrame], Dict[str, Any]]:
+
+        input_data = {}
+
+        default_values = self._read_default_values(self.config)
+
+        for parameter, details in self.config.items():
+            logger.info("Looking for %s", parameter)
+            config_details = self.config[parameter]
+
+            csv_path = os.path.join(filepath, parameter + ".csv")
+
+            try:
+                df = pd.read_csv(csv_path)
+            except pd.errors.EmptyDataError:
+                logger.error("No data found in file for %s", parameter)
+                expected_columns = config_details["indices"]
+                default_columns = expected_columns + ["VALUE"]
+                df = pd.DataFrame(columns=default_columns)
+
+            entity_type = self.config[parameter]["type"]
+
+            if entity_type == "param":
+                narrow = self._check_parameter(df, config_details, parameter)
+                if not narrow.empty:
+                    narrow_checked = check_datatypes(narrow, self.config, parameter)
+                else:
+                    narrow_checked = narrow
+            elif entity_type == "set":
+                narrow = self._check_set(df, config_details, parameter)
+                if not narrow.empty:
+                    narrow_checked = check_set_datatype(narrow, self.config, parameter)
+                else:
+                    narrow_checked = narrow
+
+            input_data[parameter] = narrow_checked
+
+        return input_data, default_values
 
 
 class ReadDatapackage(ReadStrategy):
