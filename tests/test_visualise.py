0606894bbb05533715241954c57462e08f2072d4
Will Usher <wusher@kth.se>
2020-10-06 20:55:11 +0200

    Added pytest to conda install

--- tests/test_visualise.py
+++ tests/test_visualise.py
@@ -2,13 +2,12 @@ import tempfile
 
 from otoole.visualise import create_res
 
-SIMPLICITY_VERSION = "v0.2.1"
+url = (
+    "https://zenodo.org/record/3707794/files/OSeMOSYS/simplicity-v0.2.1.zip?download=1"
+)
 
 
 def test_create_res():
 
-    url = "https://github.com/OSeMOSYS/simplicity/archive/{}.zip".format(
-        SIMPLICITY_VERSION
-    )
     _, path_to_resfile = tempfile.mkstemp(suffix=".pdf")
     create_res(url, path_to_resfile)
