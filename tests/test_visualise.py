790c9d1a38c10ee3498c6501ee960af0686a3cdf
Will Usher <wusher@kth.se>
2020-10-06 16:07:27 +0200

    Update travis and fix tests

--- tests/test_visualise.py
+++ tests/test_visualise.py
@@ -2,11 +2,13 @@ import tempfile
 
 from otoole.visualise import create_res
 
-SIMPLICITY_VERSION = 'v0.1a0'
+SIMPLICITY_VERSION = "v0.2.1"
 
 
 def test_create_res():
 
-    url = 'https://github.com/OSeMOSYS/simplicity/archive/{}.zip'.format(SIMPLICITY_VERSION)
-    _, path_to_resfile = tempfile.mkstemp(suffix='.pdf')
+    url = "https://github.com/OSeMOSYS/simplicity/archive/{}.zip".format(
+        SIMPLICITY_VERSION
+    )
+    _, path_to_resfile = tempfile.mkstemp(suffix=".pdf")
     create_res(url, path_to_resfile)
