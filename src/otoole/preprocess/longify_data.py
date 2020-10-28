1ad10e36cfa9cdb48202cbfa545000e6ecafd0eb
Will Usher <wusher@kth.se>
2020-10-28 13:04:08 +0100

    Revert "ReadStrategies cast dataframes to correct types"
    This reverts commit e3eee872a8839ecdc48ff12678bd3f33c57f629f.

--- src/otoole/preprocess/longify_data.py
+++ src/otoole/preprocess/longify_data.py
@@ -62,10 +62,14 @@ def check_datatypes(
             )
             if datatype == "int":
                 try:
-                    narrow[column] = narrow[column].apply(lambda x: int(x))
+                    narrow[column] = narrow[column].apply(_cast_to_int)
                 except ValueError as ex:
                     msg = "Unable to apply datatype for column {}: {}".format(
                         column, str(ex)
                     )
                     raise ValueError(msg)
     return narrow.astype(dtypes)
+
+
+def _cast_to_int(value):
+    return int(float(value))
