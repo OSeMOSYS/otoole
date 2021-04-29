d0f325066c1dea15f305fc33f55a5be93753b546
Will Usher <wusher@kth.se>
2021-04-29 22:52:47 +0200

    Added utility to create config from datapackage schema

---
+++ tests/test_utils.py
@@ -0,0 +1,16 @@
+from unittest.mock import MagicMock
+
+from otoole.utils import extract_config, read_packaged_file
+
+
+class TestDataPackageSchema:
+    def test_read_datapackage_schema_into_config(self):
+
+        schema = read_packaged_file("datapackage.json", "otoole.preprocess")
+        mock = MagicMock()
+        mock.__getitem__.return_value = 0
+
+        actual = extract_config(schema, mock)
+
+        expected = read_packaged_file("config.yaml", "otoole.preprocess")
+        assert actual == expected
