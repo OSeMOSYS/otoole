26fd0966e8a447c822786f8c0b5eadb67268fce7
trevorb1 <trevor_barnes@sfu.ca>
2022-12-06 18:39:49 -0800

    added tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,7 +2,7 @@ from unittest.mock import MagicMock
 
 import pytest
 
-from otoole.utils import extract_config, read_packaged_file
+from otoole.utils import create_name_mappings, extract_config, read_packaged_file
 
 
 class TestDataPackageSchema:
@@ -16,4 +16,30 @@ class TestDataPackageSchema:
         actual = extract_config(schema, mock)
 
         expected = read_packaged_file("config.yaml", "otoole.preprocess")
+        assert actual == expected
+
+
+class TestCreateNameMappings:
+    def test_create_name_mappings(self, user_config):
+        expected = {
+            "TotalAnnualMaxCapacityInvestment": "TotalAnnualMaxCapacityInvestmen",
+            "TotalAnnualMinCapacityInvestment": "TotalAnnualMinCapacityInvestmen",
+            "TotalTechnologyAnnualActivityLowerLimit": "TotalTechnologyAnnualActivityLo",
+            "TotalTechnologyAnnualActivityUpperLimit": "TotalTechnologyAnnualActivityUp",
+            "TotalTechnologyModelPeriodActivityLowerLimit": "TotalTechnologyModelPeriodActLo",
+            "TotalTechnologyModelPeriodActivityUpperLimit": "TotalTechnologyModelPeriodActUp",
+        }
+        actual = create_name_mappings(user_config)
+        assert actual == expected
+
+    def test_create_name_mappings_reversed(self, user_config):
+        expected = {
+            "TotalAnnualMaxCapacityInvestmen": "TotalAnnualMaxCapacityInvestment",
+            "TotalAnnualMinCapacityInvestmen": "TotalAnnualMinCapacityInvestment",
+            "TotalTechnologyAnnualActivityLo": "TotalTechnologyAnnualActivityLowerLimit",
+            "TotalTechnologyAnnualActivityUp": "TotalTechnologyAnnualActivityUpperLimit",
+            "TotalTechnologyModelPeriodActLo": "TotalTechnologyModelPeriodActivityLowerLimit",
+            "TotalTechnologyModelPeriodActUp": "TotalTechnologyModelPeriodActivityUpperLimit",
+        }
+        actual = create_name_mappings(user_config, map_full_to_short=False)
         assert actual == expected
